9df441c92b99255846f74c2d56242a36073f2ae0
Minervus00 <104757065+Minervus00@users.noreply.github.com>
2025-02-28 16:50:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fonction apour ajouter du texte après click sur le bouton
 document.getElementById("welcomeButton").addEventListener("click", function () {
   document.getElementById("welcomeMessage").textContent =
     "Nous sommes ravis de vous accueillir sur le site dédié à l'EPO. Explorez nos programmes et nos initiatives!";
